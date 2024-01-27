e1b8377822a0f627c6076df492e33ee9cce14f6e
NAVEEN PRAKASH <naveen.prakash.kv@gmail.com>
2024-01-27 22:15:22 +0530

    deletion done

--- basic_2.sql
+++ basic_2.sql
@@ -1,22 +1,30 @@
-SHOW DATABASES;
-use customer_record ;
+-- SHOW DATABASES;
+-- use customer_record ;
 
 
-CREATE TABLE if not exists customers (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    email VARCHAR(40) NOT NULL DEFAULT 'No email provided',
-    amount INT,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE if not exists customers (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(30) NOT NULL,
+--     email VARCHAR(40) NOT NULL DEFAULT 'No email provided',
+--     amount INT,
+--     PRIMARY KEY (id)
+-- );
 
 
-INSERT INTO customers(name, email, amount)
-VALUES ('hitesh', 'hitesh@lco.dev', 35),
-        ('George', 'geo@lco.dev', 45),
-        ('hitesh', 'hitesh@gmail.com', 88),
-        ('lina', 'lina@gmail.com', 78),
-        ('Jimmy', 'jimmy@yahoo.co.in', 54),
-        ('lina', 'lina@yahoo.co.in', 35),
-        ('hitesh', 'hitesh@yahoo.co.in', 56);
+-- INSERT INTO customers(name, email, amount)
+-- VALUES ('hitesh', 'hitesh@lco.dev', 35),
+--         ('George', 'geo@lco.dev', 45),
+--         ('hitesh', 'hitesh@gmail.com', 88),
+--         ('lina', 'lina@gmail.com', 78),
+--         ('Jimmy', 'jimmy@yahoo.co.in', 54),
+--         ('lina', 'lina@yahoo.co.in', 35),
+--         ('hitesh', 'hitesh@yahoo.co.in', 56);
 
+
+-- -- changing mail id of lina to another ie updating
+
+-- UPDATE customers set email='lina@gmail.com' where name='lina'
+
+-- delete from customers where id=4;
+
+select * FROM customers
