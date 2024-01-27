534c28b768e08f5b436a4df9860e9c525085dd69
NAVEEN PRAKASH <naveen.prakash.kv@gmail.com>
2024-01-27 21:54:35 +0530

    second data

---
+++ basic_2.sql
@@ -0,0 +1,21 @@
+SHOW DATABASES;
+use customer_record ;
+
+
+CREATE TABLE if not exists customers (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(40) NOT NULL DEFAULT 'No email provided',
+    amount INT,
+    PRIMARY KEY (id)
+);
+
+
+INSERT INTO customers(name, email, amount)
+VALUES ('hitesh', 'hitesh@lco.dev', 35),
+        ('George', 'geo@lco.dev', 45),
+        ('hitesh', 'hitesh@gmail.com', 88),
+        ('lina', 'lina@gmail.com', 78),
+        ('Jimmy', 'jimmy@yahoo.co.in', 54),
+        ('lina', 'lina@yahoo.co.in', 35),
+        ('hitesh', 'hitesh@yahoo.co.in', 56);
