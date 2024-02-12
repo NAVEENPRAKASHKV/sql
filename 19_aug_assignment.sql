c1efc1ba81a11c1c9dc54e9e1f350f9a7bf186c5
NAVEEN PRAKASH <naveen.prakash.kv@gmail.com>
2024-02-12 22:18:31 +0530

    17 th completed

--- 19_aug_assignment.sql
+++ 19_aug_assignment.sql
@@ -679,9 +679,19 @@ create table IF NOT EXISTS  station (Id INT,City VARCHAR(21), State VARCHAR(2),L
                 WHERE CITY NOT REGEXP '^[aeiouAEIOU]' and CITY NOT REGEXP '[aeiouAEIOU]$';
 
 
--- Q17
+-- Q17 Write an SQL query that reports the products that were only sold in the first quarter of 2019. That is, between 2019-01-01 and 2019-03-31 inclusive.
 
-               
+                create table if not EXISTS product(product_id int primary key ,product_name varchar(40),unit_price int);
+                create table if not EXISTS sales(seller_id int ,product_id int,buyer_id int,sale_date date,quantity int,price int);
+
+
+                --  INSERT INTO product(product_id,product_name,unit_price) values (1,'S8',1000),(2,'G4',800),(3,'iPhone',1400);
+
+                -- INSERT INTO sales(seller_id,product_id,buyer_id,sale_date,quantity,price) values (1,1,1,'2019-01-21',2,2000),(1,2,2,'2019-02-17',1,800),(2,2,3,'2019-06-02',1,800),(3,3,4,'2019-05-13',2,2800);
+
+
+                SELECT p.product_id,p.product_name from product as p
+                JOIN sales as s on p.product_id=s.product_id where sale_date BETWEEN '2019-01-01' and '2019-03-31';
 
 
 
