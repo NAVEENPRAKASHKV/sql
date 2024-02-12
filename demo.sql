52f91e07a617a7ceb3cb78fcc27f7e857da63dec
NAVEEN PRAKASH <naveen.prakash.kv@gmail.com>
2024-02-12 21:19:55 +0530

    coddeded in demo

--- demo.sql
+++ demo.sql
@@ -1,4 +1,4 @@
+-- source demo.sql
 
--- Q5. Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
-
-SELECT * from country WHERE COUNTRYCODE ="JPN"
+create table if not EXISTS product(product_id int,product_name varchar(40),unit_price int);
+create table if not EXISTS sales(seller_id int ,product_id int,buyer_id int,sale_date date,quantity int,price int);
